cc665bd3523decefe6367e78740b890699bfaa96
Karim Boucher <boucherkarim@gmail.com>
2019-12-17 16:35:18 -0500

    Affect all MacOS versions > 10.12

--- electrum/plugins/cosigner_pool/__init__.py
+++ electrum/plugins/cosigner_pool/__init__.py
@@ -21,8 +21,7 @@ if sys.platform == 'darwin':
     context.check_hostname = True
     v, _, _ = platform.mac_ver()
     v = float('.'.join(v.split('.')[:2]))
-    release = [10.12, 10.13, 10.14, 10.15]
-    if v in release:
+    if v > 10.12:
         context.load_verify_locations(cafile='/private/etc/ssl/cert.pem')    
 
 server = ServerProxy('https://cosigner.exos.to/', allow_none=True, verbose=False, use_datetime=True, context=context)
